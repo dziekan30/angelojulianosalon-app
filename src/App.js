1ce296a83b1dafd0ac23eec6eb7d47f64be535b9
Kamil Dziekan <dziekan30@gmail.com>
2020-10-29 19:37:14 -0500

    playing with google map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home'
+import About from './components/About'
 import Footer from './components/Footer'
 import ContactUs from './components/ContactUs'
 import Navbarr from './components/Navbarr'
@@ -14,7 +15,7 @@ function App() {
         <Switch>
           {/* <Route path="/home" component={Home} /> */}
           <Route exact path="/home" component={Home} />
-          {/* <Route path="/about" component={About} /> */}
+          <Route path="/about" component={About} />
           <Route path="/contact" component={ContactUs} />
           <Route path="/" component={Home} />
           <Route component={Error} />
